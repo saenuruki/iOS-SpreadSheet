b9223ac1be49ba6bf178a1e0f051a001ba700b1e
“saenuruki” <sae.webwork@gmail.com>
2020-01-26 17:26:12 +0900

    add tableCell

---
+++ EvangVitalData/EvangVitalData/Exam/ExamTableCell.swift
@@ -0,0 +1,88 @@
+//
+//  ExamTableCell.swift
+//  EvangVitalData
+//
+//  Created by 塗木冴 on 2020/01/26.
+//  Copyright © 2020 塗木冴. All rights reserved.
+//
+
+import UIKit
+
+class ExamTableCell: UITableViewCell {
+
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    @IBOutlet weak var fourthView: UIView!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var fourthLabel: UILabel!
+    
+    lazy var allButtons: [UIButton?] = {
+        let buttons = [firstButton, secondButton, thirdButton, fourthButton]
+        return buttons
+    }()
+    lazy var allViews: [UIView?] = {
+        let views = [firstView, secondView, thirdView, fourthView]
+        return views
+    }()
+    lazy var allLabels: [UILabel?] = {
+        let labels = [firstLabel, secondLabel, thirdLabel, fourthLabel]
+        return labels
+    }()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        confgireUI()
+//        configureButton()
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
+//    func configure(diary: Diary) {
+//
+//        dateLabel.text = diary.date
+//        contentLabel.text = diary.text
+//    }
+    
+    @IBAction func tapFirstButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapSecondButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapThirdButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapFourthButton(_ sender: Any) {
+    }
+}
+
+fileprivate extension ExamTableCell {
+
+    func confgireUI() {
+        cardView.layer.cornerRadius = 8.0
+        cardView.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        cardView.layer.shadowOpacity = 0.2
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowRadius = 5.0
+        
+        allViews.forEach {
+            guard let view = $0 else { return }
+            view.layer.cornerRadius = 2
+            view.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+            view.layer.shadowOpacity = 0.2
+            view.layer.shadowColor = UIColor.black.cgColor
+            view.layer.shadowRadius = 2.0
+        }
+    }
+}
