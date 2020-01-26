5b8acad0f824f67573fc1e79c2806e3bde9a84bb
“saenuruki” <sae.webwork@gmail.com>
2020-01-26 16:21:56 +0900

    create project

---
+++ EvangVitalData/EvangVitalData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EvangVitalData
+//
+//  Created by 塗木冴 on 2020/01/26.
+//  Copyright © 2020 塗木冴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
